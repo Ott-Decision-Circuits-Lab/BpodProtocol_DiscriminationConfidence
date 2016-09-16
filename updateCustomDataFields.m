73ab23ccbff3d8528d939a4101a348eb1cf18030
Torben Ott <tott@cshl.edu>
2016-09-16 13:33:18 -0400

    syntax typo

--- updateCustomDataFields.m
+++ updateCustomDataFields.m
@@ -131,7 +131,7 @@ if TaskParameters.GUI.MinSampleAudAutoincrement
         elseif mean(BpodSystem.Data.Custom.ST(ConsiderTrials)>TaskParameters.GUI.MinSampleAud) < Crit/2
             if BpodSystem.Data.Custom.EarlyWithdrawal(iTrial)
                 TaskParameters.GUI.MinSampleAud = max(TaskParameters.GUI.MinSampleAudMin,...
-                	in(TaskParameters.GUI.MinSampleAudMax,BpodSystem.Data.Custom.MinSampleAud(iTrial) - TaskParameters.GUI.MinSampleAudDecr));
+                	min(TaskParameters.GUI.MinSampleAudMax,BpodSystem.Data.Custom.MinSampleAud(iTrial) - TaskParameters.GUI.MinSampleAudDecr));
             end
         else
             TaskParameters.GUI.MinSampleAud = BpodSystem.Data.Custom.MinSampleAud(iTrial);
