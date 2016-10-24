b2dfd8e7f9511bfc0ccd6e6944421e4267a35f0b
Thiago Gouvea (@Bob) <tsgouvea@cshl.edu>
2016-10-24 16:06:50 -0400

    Plays feedback sounds in both audio channels simultaneously

--- SoftCodeHandler.m
+++ SoftCodeHandler.m
@@ -34,12 +34,14 @@ if softCode > 10 && softCode < 21 %for auditory
         if softCode == 11 %noise on chan 1
             ProgramPulsePal(BpodSystem.Data.Custom.PulsePalParamFeedback);
             SendCustomPulseTrain(1,cumsum(randi(9,1,601))/10000,(rand(1,601)-.5)*20); % White(?) noise on channel 1
-            TriggerPulsePal(1);
+            SendCustomPulseTrain(2,cumsum(randi(9,1,601))/10000,(rand(1,601)-.5)*20);
+            TriggerPulsePal(1,2);
             ProgramPulsePal(BpodSystem.Data.Custom.PulsePalParamStimulus);
         elseif softCode == 12 %beep on chan 2
             ProgramPulsePal(BpodSystem.Data.Custom.PulsePalParamFeedback);
             SendCustomPulseTrain(2,0:.001:.3,(ones(1,301)*3));  % Beep on channel 2
-            TriggerPulsePal(2);
+            SendCustomPulseTrain(1,0:.001:.3,(ones(1,301)*3));
+            TriggerPulsePal(1,2);
             ProgramPulsePal(BpodSystem.Data.Custom.PulsePalParamStimulus);
         end
     end
