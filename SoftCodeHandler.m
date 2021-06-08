1994622e4f6102600b4290afb8aa7d0ee4a78278
Suelynn <sren1340@gmail.com>
2021-06-08 17:20:27 -0500

    updateSoftCode

--- SoftCodeHandler.m
+++ SoftCodeHandler.m
@@ -44,7 +44,13 @@ if softCode > 10 && softCode < 21 %for auditory clicks
             SendCustomPulseTrain(2,0:.001:.3,(ones(1,301)*3));  % Beep on channel 1+2
             SendCustomPulseTrain(1,0:.001:.3,(ones(1,301)*3));
             TriggerPulsePal(1,2);
-            ProgramPulsePal(BpodSystem.Data.Custom.PulsePalParamStimulus);            
+            ProgramPulsePal(BpodSystem.Data.Custom.PulsePalParamStimulus);
+        elseif softCode == 13 %beep on chan 2
+            ProgramPulsePal(BpodSystem.Data.Custom.PulsePalParamFeedback);
+            SendCustomPulseTrain(2,0:.001:.3,(ones(1,301)*0.2));  % Beep on channel 1+2
+            SendCustomPulseTrain(1,0:.001:.3,(ones(1,301)*0.2));
+            TriggerPulsePal(1,2);
+            ProgramPulsePal(BpodSystem.Data.Custom.PulsePalParamStimulus);
         end
     end
 end
