9f2cacff9adb16f34a0d6f8479056e830320182b
OttLabIT <116281282+OttLabIT@users.noreply.github.com>
2022-12-05 17:14:23 +0100

    Update LoadIndependentWaveform.m
    In case TimeOut being set as 0, no error will be made for loading empty Waveform

--- LoadIndependentWaveform.m
+++ LoadIndependentWaveform.m
@@ -6,12 +6,20 @@ fs = Player.SamplingRate;
 
 PunishSound = rand(1, fs*TaskParameters.GUI.TimeOutEarlyWithdrawal)*2 - 1;
 SoundIndex = 1;
-Player.loadWaveform(SoundIndex, PunishSound);
+try
+    Player.loadWaveform(SoundIndex, PunishSound);
+catch
+    fprintf('Error: Punish Sound not loaded, probably TimeOutEarlyWithdrawal set as 0.');
+end
 
 SoundLevel = 0.8;
 ErrorSound = rand(1, fs*TaskParameters.GUI.TimeOutIncorrectChoice)*2 - 1; 
 % ErrorSound = ErrorSound * SoundLevel;
 SoundIndex = 2;
-Player.loadWaveform(SoundIndex, ErrorSound);
+try
+    Player.loadWaveform(SoundIndex, ErrorSound);
+catch
+    fprintf('Error: Error Sound not loaded, probably TimeOutIncorrectChoice set as 0.');
+end
 
 end
